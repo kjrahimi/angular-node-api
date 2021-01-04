cf0e4f1aec4850a9b72804f1329c6ea2c56914c5
Kaveh Rahimi <kavehrahimi@KavehRahimi-252.local>
2021-01-04 12:53:26 -1000

    Angular node-api component

--- angular-se-node-api/src/app/app.module.ts
+++ angular-se-node-api/src/app/app.module.ts
@@ -8,17 +8,43 @@ import { NodeApiComponent } from './node-api/node-api.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    NodeApiComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
-  declarations: [
-    AppComponent,
-    NodeApiComponent
-  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  courses = [
+    {
+      id: 1,
+      title: 'Angular 9 Fundamentals',
+      description: 'Learn the fundamentals of Angular 9',
+      percentComplete: 26,
+      favorite: true
+    },
+    {
+      id: 2,
+      title: 'Angular 9 Fundamentals 2',
+      description: 'Learn the fundamentals of Angular 9 2',
+      percentComplete: 21,
+      favorite: false
+    },
+    {
+      id: 3,
+      title: 'Angular 9 Fundamentals 3',
+      description: 'Learn the fundamentals of Angular 9 3',
+      percentComplete: 98,
+      favorite: false
+    }
+  ];
+
+}
