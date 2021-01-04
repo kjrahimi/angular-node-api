578419f6b0233ebde2e3b4cabd53494ba8842673
Kaveh Rahimi <kavehrahimi@KavehRahimi-252.local>
2021-01-04 08:41:25 -1000

    Creation of node-api component

---
+++ angular-se-node-api/src/app/node-api/node-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-api',
+  templateUrl: './node-api.component.html',
+  styleUrls: ['./node-api.component.scss']
+})
+export class NodeApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
